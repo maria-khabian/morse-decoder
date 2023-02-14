c0fa101455c3ab2b63e64bd4bac71da7bfdaa400
maria-khabian <mariakhabian@gmail.com>
2023-02-14 18:40:21 +0100

    feat: solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  const exprArr = []
+  const obj = {
+    '10': '.',
+    '11': '-',
+    '00': '',
+    '**': ' '
+  }
+  for(let i=0; i < expr.length; i += 10) {
+    exprArr.push(expr.slice(i, (i+10)))
+  }
+  const arrRes = []
+  const arr = exprArr.map((word) =>  {
+    const microArr = []
+    for(let i=0; i < word.length; i+=2) {
+      microArr.push(word.slice(i, i+2))
+    }    
+    arrRes.push(microArr)
+  } )
+  const arrMorze = arrRes.map((elem) => elem.map((elemik, i) => elemik=obj[elemik])).map((word) => word.join(''))
+  const result = arrMorze.map(word => word !== '     '? word = MORSE_TABLE[word] : word = ' ').join('')
+  return result
 }
 
 module.exports = {
